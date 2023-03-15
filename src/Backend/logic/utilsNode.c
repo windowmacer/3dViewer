b325f5eb28bd1e8645c93584d54658abb0a73046
Ivan Kudr <Ivan@Ivans-MacBook-Pro.local>
2023-03-15 04:39:08 +0300

    clean_logic

--- src/Backend/logic/utilsNode.c
+++ src/Backend/logic/utilsNode.c
@@ -42,60 +42,60 @@ void	closeCircularList(vertex *head, vertex *tail) {
 	head->prev = tail;
 }
 
-// void	findMinMax(structRoot *root, double **coord, char c) {
-// 	vertex *current = root->head;
-// 	double compare;
-// 	double min;
-// 	double max;
+void	findMinMax(structRoot *root, double **coord, char c) {
+	vertex *current = root->head;
+	double compare;
+	double min;
+	double max;
 
-// 	if (c == 'x') {
-// 		min = current->x;
-// 		max = current->x;
-// 	}
-// 	else if (c == 'y') {
-// 		min = current->y;
-// 		max = current->y;
-// 	}
-// 	else if (c == 'z') {
-// 		min = current->z;
-// 		max = current->z;
-// 	}
+	if (c == 'x') {
+		min = current->x;
+		max = current->x;
+	}
+	else if (c == 'y') {
+		min = current->y;
+		max = current->y;
+	}
+	else if (c == 'z') {
+		min = current->z;
+		max = current->z;
+	}
 
-// 	for (int32_t i = 1; i < root->countVertex; i++) {
-// 		if (c == 'x')
-// 			compare = current->x;
-// 		else if (c == 'y')
-// 			compare = current->y;
-// 		else if (c == 'z')
-// 			compare = current->z;
-// 		if (min > compare)
-// 			min = compare;
-// 		if (max < compare)
-// 			max = compare;
-// 		current = current->next;
-// 	}
-// 	*coord[0] = min;
-// 	*coord[1] = max;
-// }
+	for (int32_t i = 1; i < root->countVertex; i++) {
+		if (c == 'x')
+			compare = current->x;
+		else if (c == 'y')
+			compare = current->y;
+		else if (c == 'z')
+			compare = current->z;
+		if (min > compare)
+			min = compare;
+		if (max < compare)
+			max = compare;
+		current = current->next;
+	}
+	*coord[0] = min;
+	*coord[1] = max;
+}
 
-// void	findCenter(structRoot *root) {
-// 	vertex *current;
-// 	double 	center[3];
-// 	double 	coord_x[2];
-// 	double 	coord_y[2];
-// 	double 	coord_z[2];
+void	findCenter(structRoot *root) {
+	vertex *current;
+	double 	center[3];
+	double 	coord_x[2];
+	double 	coord_y[2];
+	double 	coord_z[2];
 
-// 	findMinMax(root, &coord_x, 'x');
-// 	findMinMax(root, &coord_y, 'y');
-// 	findMinMax(root, &coord_z, 'z');
-// 	center[0] = coord_x[0] + (coord_x[1] - coord_x[0]) / 2;
-// 	center[1] = coord_y[0] + (coord_y[1] - coord_y[0]) / 2;
-// 	center[2] = coord_z[0] + (coord_z[1] - coord_z[0]) / 2;
+	findMinMax(root, &coord_x, 'x');
+	findMinMax(root, &coord_y, 'y');
+	findMinMax(root, &coord_z, 'z');
+	center[0] = coord_x[0] + (coord_x[1] - coord_x[0]) / 2;
+	center[1] = coord_y[0] + (coord_y[1] - coord_y[0]) / 2;
+	center[2] = coord_z[0] + (coord_z[1] - coord_z[0]) / 2;
 
-// 	current = root->head;
-// 	for (int32_t i = 1; i < root->countVertex; i++) {
-// 		current->x -= center[0];
-// 		current->y -= center[1];
-// 		current->z -= center[2];
-// 	}
-// }
+	current = root->head;
+	for (int32_t i = 1; i < root->countVertex; i++) {
+		current->x -= center[0];
+		current->y -= center[1];
+		current->z -= center[2];
+	}
+}
