ef3b7063cb5447acd2e1821b7733a2c56ee99b91
Lunarskii <lunarskii0@gmail.com>
2023-03-26 10:09:01 +0300

    added affine transformations

--- src/Backend/logic/affine_transformations.c
+++ src/Backend/logic/affine_transformations.c
@@ -1,1 +1,43 @@
-#include "headerCAD.h"
+#include <stdio.h>
+#include <math.h>
+
+#include "parser_obj.h"
+
+#define PI 3.14159265358979323846f
+
+#define X 1
+#define Y 2
+#define Z 3
+
+void moveModel(structRoot *model, double stepX, double stepY, double stepZ) {
+    for (int i = 0; i < model->countVertex * 3; i += 3) {
+        model->vertexCoord[i] += stepX;
+        model->vertexCoord[i + 1] += stepY;
+        model->vertexCoord[i + 2] += stepZ;
+    }
+}
+
+void rotateModel(structRoot *model, double angle, int axis) {
+    angle *= PI / 180;
+    double cosValue = cos(angle);
+    double sinValue = sin(angle);
+    
+    for (int i = (axis == X) ? 1 : 0; i < model->countVertex * 3; i += 3) {
+        if (axis == X) {
+            model->vertexCoord[i]     = cosValue * model->vertexCoord[i] - sinValue * model->vertexCoord[i + 1];
+            model->vertexCoord[i + 1] = sinValue * model->vertexCoord[i] + cosValue * model->vertexCoord[i + 1];
+        } else if (axis == Y) {
+            model->vertexCoord[i]     = cosValue * model->vertexCoord[i] + sinValue * model->vertexCoord[i + 2];
+            model->vertexCoord[i + 2] =-sinValue * model->vertexCoord[i] + cosValue * model->vertexCoord[i + 2];
+        } else if (axis == Z) {
+            model->vertexCoord[i]     = cosValue * model->vertexCoord[i] - sinValue * model->vertexCoord[i + 1];
+            model->vertexCoord[i + 1] = sinValue * model->vertexCoord[i] + cosValue * model->vertexCoord[i + 1];
+        }
+    }
+}
+
+void scaleModel(structRoot *model, double scale) {
+    for (int i = 0; i < model->countVertex; i++) {
+        model->vertexCoord[i] += scale / 100 * model->vertexCoord[i];
+    }
+}
