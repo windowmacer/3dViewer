4cde49a77d46f823a2337762d6e9784a94f6be03
Lunarskii <lunarskii0@gmail.com>
2023-03-27 21:50:39 +0300

    prod

--- src/install.sh
+++ src/install.sh
@@ -4,7 +4,6 @@ DIR=""
 echo "Введите директорию для установки приложения"
 read DIR
 
-export VIEWER_INSTALLATION_FOLDER=$DIR
 if [ "$DIR" == "" ]; then
     cmake -S GUI/3DViewer -B build
     make -C build
--- src/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [rm -rf $VIEWER_INSTALLATION_FOLDER/build]
-then
-	unset VIEWER_INSTALLATION_FOLDER
-	echo "Директория установки 3DViewer успешно удалена"
-fi
