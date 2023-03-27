342902e6765bd17156cc18005bb05eb0dd0808be
Kudresdss <iankudr@gmail.com>
2023-03-27 20:53:33 +0300

    updated scripts

--- src/install.sh
+++ src/install.sh
@@ -5,6 +5,7 @@ NAME=""
 echo "Введите директорию для установки приложения"
 read NAME
 
+export VIEWER_INSTALLATION_FOLDER=$NAME
 mv $LIBRARY GUI/3DViewer/lib$LIBRARY
 cmake -S GUI/3DViewer -B $NAME/build
 make -C $NAME/build
---
+++ src/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+if rm -rf $VIEWER_INSTALLATION_FOLDER/build
+	echo "Директория установки 3DViewer успешно удалена"
