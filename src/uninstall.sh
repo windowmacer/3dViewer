bd255e89b275b39955dc88ecf6b14053c76c9f35
Kudresdss <iankudr@gmail.com>
2023-03-27 21:16:24 +0300

    uninstall script done

--- src/uninstall.sh
+++ src/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if rm -rf $VIEWER_INSTALLATION_FOLDER/build
+if [rm -rf $VIEWER_INSTALLATION_FOLDER/build]
 then
 	unset VIEWER_INSTALLATION_FOLDER
 	echo "Директория установки 3DViewer успешно удалена"
